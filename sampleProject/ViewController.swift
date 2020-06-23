3ce8121dd78ba1d6959785c9d637a0ed1d0f765d
veeresham <veeresham@veereshams-MacBook-Pro.local>
2020-06-23 11:07:59 +0530

    first commit

---
+++ sampleProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sampleProject
+//
+//  Created by veeresham on 6/23/20.
+//  Copyright © 2020 lanciusit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print ("Hello World")
+    }
+
+
+}
+
